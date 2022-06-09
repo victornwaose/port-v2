75688a471efebc42de5e5cf4eb8301bbf6d4f336
nwaose victor <nwaosevictor1234@gmail.com>
2022-06-09 02:46:12 +0100

    added styles

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -3,7 +3,7 @@ import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const Socials = () => {
     return (
-        <div>
+        <div className="flex items-center relative z-20 p-5 w-9/12 mx-auto justify-between md:justify-start md:fixed bottom-0 left-0 py-0 px-8 w-auto flex-col">
             <FaGithub />
             <FaTwitter />
             <FaLinkedin />
