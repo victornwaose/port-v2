4f2d0f341351e654701f3052f4667231342093a5
nwaose victor <nwaosevictor1234@gmail.com>
2022-06-13 22:27:44 +0100

    changes

--- src/components/email_comp/Email.jsx
+++ src/components/email_comp/Email.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Email = () => {
     return (
-        <div className="text-center text-xs relative z-20 py-5 w-[40%] mx-auto items-center flex md:text-base md:py-0 md:justify-start md:fixed md:bottom-0 md:right-0 md:px-8 md:w-auto md:flex-col md:h-3/5">
+        <div className="text-center text-xs relative z-20 py-5 w-auto mx-auto items-center flex md:text-base md:py-0 md:justify-start md:fixed md:bottom-0 md:right-0 md:px-5  md:w-auto md:flex-col md:h-3/5">
             <h1
                 className="text-gray-400 hover:text-emerald-300 font-mono md:rotate-180"
                 style={{ writingMode: "vertical-rl" }}
--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -3,7 +3,7 @@ import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const Socials = () => {
     return (
-        <div className="flex items-center flex-row relative z-20 p-5 w-auto mx-auto  justify-between md:justify-start md:fixed md:bottom-0 md:left-0 md:my-72  md:py-0 md:px-5 md:w-auto md:flex-col">
+        <div className="flex items-center flex-row relative z-20 p-5 w-auto mx-auto  justify-between md:justify-start md:fixed md:bottom-0 md:left-0 md:my-72  md:py-0 md:px-2 md:w-auto md:flex-col">
             <FaGithub className="w-[60%] h-10 text-gray-400 hover:text-emerald-300" />
             <FaTwitter className="w-[60%] h-10 text-gray-400 hover:text-emerald-300" />
             <FaLinkedin className="w-[60%] h-10 text-gray-400 hover:text-emerald-300" />
