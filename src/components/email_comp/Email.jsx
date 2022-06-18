246d3d44421f70d649ac0cc10584975f95b1cecb
nwaose victor <nwaosevictor1234@gmail.com>
2022-06-18 21:15:28 +0100

    changes

--- src/components/email_comp/Email.jsx
+++ src/components/email_comp/Email.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 const Email = () => {
+    const [dimensions, setDimensions] = React.useState({
+        width: window.innerWidth,
+    });
+
+    const handleResize = () => {
+        setDimensions({
+            width: window.innerWidth,
+        });
+    };
+    React.useEffect(() => {
+        window.addEventListener("resize", handleResize, false);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
     return (
         <div className="text-center text-xs relative z-20 py-5 w-auto mx-auto items-center flex md:text-base md:py-0 md:justify-start md:fixed md:bottom-0 md:right-0 md:px-5  md:w-auto md:flex-col md:h-3/5">
             <h1
-                className="text-gray-400 hover:text-emerald-300 font-mono md:rotate-180"
-                style={{ writingMode: "vertical-rl" }}
+                className="text-gray-400 rotate-none hover:text-emerald-300 font-mono md:rotate-180"
+                style={
+                    dimensions.width > 768
+                        ? { writingMode: "vertical-rl" }
+                        : { writingMode: "horizontal-tb" }
+                }
             >
                 nwaosevictior1234@gmail.com
             </h1>
